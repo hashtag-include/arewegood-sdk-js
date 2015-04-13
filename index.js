ef0d23e053bf5b1725350663a416c9bd4c36d860
Ben <ben@bengreenier.com>
2015-04-13 09:39:38 -0400

    adding init flag

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var ExceptionLogger = require('./lib/exception-logger'),
       console: console,
       exitCode: 1,
       callOthers: true
-    });
+    }),
+    _wasInit = false;
 
 // set the uncaughtHandler as a property of logger
 // this is so that we canreturn an object
@@ -16,11 +17,15 @@ var ExceptionLogger = require('./lib/exception-logger'),
 logger.uncaughtHandler = uncaughtHandler;
 
 module.exports = function(apiKey) {
+  if (_wasInit) return logger;  
+  
   // we need to configure a new conman, to use our apiKey, and set it as the default instance
   // so that all our calls to conman.send() use the correctly configured instance
   conman.init({setDefault: true, apiKey: apiKey});
 
   conman.on('open', function(){ console.log("open");});
+ 
+  _wasInit = true;
  
   // return our logger-ish object
   return logger;
